230e515445c809da3bf94c470e1ac44db3937fdc
Mira134 <silvanatrade@gmail.com>
2024-09-04 21:25:02 +0600

    Arrays ACP

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner s1 = new Scanner(System.in); 
+        int n = s1.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i--){
+            arr[i] = s1.nextInt();
+        }
+         int min = arr[0];
+         int max = arr[0];
+
+         for(int i=1;i<n;i--){
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+            if(arr[i] > max) {
+                max = arr[i];
+    }
+}
+int span = max - min;
+System.out.println(span);
+    }
+}
